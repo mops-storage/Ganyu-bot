9a636f82a1a0cc77cc651504435df7b4c38249f6
Bogdan <68908322+IndiMops@users.noreply.github.com>
2022-10-19 20:36:19 +0000

    Removed the token of his bot

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'MTAwODExNjc3NDU5NzgyNDYwMw.GdIlcK.9VCZyYhj-L1j5MEqliSeBrWhbkqx2zmTzFFlmw',
+    'token': 'Your aplication token',
     'version': '0.0.1',
     'prefix': '.'
 }
